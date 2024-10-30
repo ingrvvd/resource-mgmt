920dfbc85a44a598d781bbd83a54c55e2ba38d04
ingrvvd <ingridteo01@gmail.com>
2024-10-30 11:42:23 +0800

    Created the base Node.js project

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+export const app = express();
+const PORT = process.env.PORT || 5050;
+const startPage = "index.html";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/" + startPage);
+});
+
+export const server = app.listen(PORT, () => {
+  const address = server.address();
+
+  // Ensure address is not null and has the correct type
+  if (address && typeof address !== "string") {
+    const baseUrl = `http://${address.address === "::"
+      ? "localhost"
+      : address.address}:${address.port}`;
+    console.log(`Demo project at: ${baseUrl}`);
+  } else {
+    console.error("Server address is not available");
+  }
+});
