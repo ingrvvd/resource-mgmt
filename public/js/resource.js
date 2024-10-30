235c6937892634ead83cd331333e832812b5bcb8
ingrvvd <ingridteo01@gmail.com>
2024-10-30 17:00:58 +0800

    Implemented the frontend for modify-resource

--- public/js/resource.js
+++ public/js/resource.js
@@ -53,7 +53,7 @@ const viewResources = () => {
   var request = new XMLHttpRequest();
   request.open("GET", "/view-resources", true);
   request.setRequestHeader("Content-Type", "application/json");
-  request.onload = function() {
+  request.onload = () => {
     response = JSON.parse(request.responseText);
     var html = "";
     for (var i = 0; i < response.length; i++) {
@@ -85,6 +85,77 @@ const viewResources = () => {
         "</tr>";
     }
     document.getElementById("tableContent").innerHTML = html;
+  };
+  request.send();
+};
+
+const editResource = data => {
+  var selectedResource = JSON.parse(data);
+  document.getElementById("editName").value = selectedResource.name;
+  document.getElementById("editLocation").value = selectedResource.location;
+  document.getElementById("editDescription").value =
+    selectedResource.description;
+  document.getElementById("editOwner").value = selectedResource.owner;
+  document
+    .getElementById("updateButton")
+    .setAttribute("onclick", 'updateResource("' + selectedResource.id + '")');
+  $("#editResourceModal").modal("show");
+};
+
+const updateResource = id => {
+  console.log(id);
+  var response = "";
+  var jsonData = new Object();
+  jsonData.name = document.getElementById("editName").value;
+  jsonData.location = document.getElementById("editLocation").value;
+  jsonData.description = document.getElementById("editDescription").value;
+  jsonData.owner = document.getElementById("editOwner").value;
+  if (
+    jsonData.name == "" ||
+    jsonData.location == "" ||
+    jsonData.description == "" ||
+    jsonData.owner == ""
+  ) {
+    document.getElementById("editMessage").innerHTML =
+      "All fields are required!";
+    document.getElementById("editMessage").setAttribute("class", "text-danger");
+    return;
+  }
+  var request = new XMLHttpRequest();
+  request.open("PUT", "/edit-resource/" + id, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.onload = () => {
+    response = JSON.parse(request.responseText);
+    if (response.message == "Resource modified successfully!") {
+      document.getElementById("editMessage").innerHTML =
+        "Edited Resource: " + jsonData.name + "!";
+      document
+        .getElementById("editMessage")
+        .setAttribute("class", "text-success");
+      window.location.href = "index.html";
+    } else {
+      document.getElementById("editMessage").innerHTML =
+        "Unable to edit resource!";
+      document
+        .getElementById("editMessage")
+        .setAttribute("class", "text-danger");
+    }
+  };
+  request.send(JSON.stringify(jsonData));
+};
+
+const deleteResource = (selectedId) => {
+  var response = "";
+  var request = new XMLHttpRequest();
+  request.open("DELETE", "/delete-resource/" + selectedId, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.onload = () => {
+    response = JSON.parse(request.responseText);
+    if (response.message == "Resource deleted successfully!") {
+      window.location.href = "index.html";
+    } else {
+      alert("Unable to delete resource!");
+    }
   };
   request.send();
 }
